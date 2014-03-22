04040bc82e3fca44daf39ff04ed94b072fcf23a5
= <cc2681a@student.american.edu>
2014-03-22 13:08:25 -0400

    Not saving

--- src/edu/american/toddlerselfie/FullscreenActivity.java
+++ src/edu/american/toddlerselfie/FullscreenActivity.java
@@ -1,17 +1,9 @@
 package edu.american.toddlerselfie;
 
-import java.io.ByteArrayOutputStream;
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-
 import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.os.Bundle;
-import android.os.Environment;
-import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.ImageView;
@@ -25,7 +17,7 @@ import edu.american.toddlerselfie.util.SystemUiHider;
  */
 public class FullscreenActivity extends Activity {
 	private static final int CAMERA_PIC_REQUEST = 1111;
-	
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -44,24 +36,11 @@ public class FullscreenActivity extends Activity {
 	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
 		if (requestCode == CAMERA_PIC_REQUEST) {
 			Bitmap thumbnail = (Bitmap) data.getExtras().get("data");
-			ByteArrayOutputStream bytes = new ByteArrayOutputStream();
-			thumbnail.compress(Bitmap.CompressFormat.JPEG, 100, bytes);
 			thumbnail = Bitmap.createScaledBitmap(thumbnail, 480, 480, true);
-			File file = new File(Environment.getExternalStorageDirectory() + File.separator + "image.jpg");
-			try {
-				file.createNewFile();
-				FileOutputStream fo = new FileOutputStream(file);
-				fo.write(bytes.toByteArray());
-				fo.close();
-				Bitmap b = BitmapFactory.decodeFile(file.getAbsolutePath());
-				ImageView v = (ImageView) findViewById(R.id.picture);
-				v.setImageBitmap(b);
-				findViewById(R.id.start).setVisibility(View.INVISIBLE);
-			} catch (IOException e) {
-				Log.wtf(this.getClass().getSimpleName(), "Could not save image", e);
-			}
+			ImageView v = (ImageView) findViewById(R.id.picture);
+			v.setImageBitmap(thumbnail);
+			findViewById(R.id.start).setVisibility(View.INVISIBLE);
 		}
-
 	}
 
 }
