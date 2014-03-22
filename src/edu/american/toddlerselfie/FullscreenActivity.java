3bbabefc2a93f7e7cc858dc7a79b146c6a80a39f
Bree Devries <bd2595a@student.american.edu>
2014-03-22 14:07:07 -0400

    added framework for dialogue box

--- src/edu/american/toddlerselfie/FullscreenActivity.java
+++ src/edu/american/toddlerselfie/FullscreenActivity.java
@@ -1,6 +1,8 @@
 package edu.american.toddlerselfie;
 
 import android.app.Activity;
+import android.app.Dialog;
+import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.os.Bundle;
@@ -17,10 +19,12 @@ import edu.american.toddlerselfie.util.SystemUiHider;
  */
 public class FullscreenActivity extends Activity {
 	private static final int CAMERA_PIC_REQUEST = 1111;
+	private Context context;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		context=this;
 		setContentView(R.layout.activity_fullscreen);
 		View v = findViewById(R.id.start);
 		v.setOnClickListener(new OnClickListener() {
@@ -31,15 +35,30 @@ public class FullscreenActivity extends Activity {
 				startActivityForResult(intent, CAMERA_PIC_REQUEST);
 			}
 		});
+		//findViewById(R.id.backButton).setVisibility(View.INVISIBLE);
+		
+		findViewById(R.id.backButton).setOnClickListener(new OnClickListener() {
+			 
+			  @Override
+			  public void onClick(View arg0) {
+	 
+				// custom dialog
+				final Dialog dialog = new Dialog(context);
+				dialog.setContentView(R.layout.puzzle_layout);
+				dialog.setTitle("Options");	 
+				dialog.show();
+			  }
+			});
 	}
 
 	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
 		if (requestCode == CAMERA_PIC_REQUEST) {
 			Bitmap thumbnail = (Bitmap) data.getExtras().get("data");
-			thumbnail = Bitmap.createScaledBitmap(thumbnail, 480, 480, true);
+			thumbnail = Bitmap.createScaledBitmap(thumbnail, 700, 480, true);
 			ImageView v = (ImageView) findViewById(R.id.picture);
 			v.setImageBitmap(thumbnail);
 			findViewById(R.id.start).setVisibility(View.INVISIBLE);
+			findViewById(R.id.backButton).setVisibility(View.VISIBLE);
 		}
 	}
 
