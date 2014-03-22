0c10482c20061a909db80930c78b3a31d5166812
= <cc2681a@student.american.edu>
2014-03-22 11:57:44 -0400

    Added Camera Helper

---
+++ src/edu/american/toddlerselfie/CameraHelper.java
@@ -0,0 +1,50 @@
+package edu.american.toddlerselfie;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
+import android.widget.ImageView;
+
+public class CameraHelper extends Activity {
+
+	private static final int CAMERA_PIC_REQUEST = 1111;
+	private ImageView mImage;
+
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.camera_fullscreen);
+
+		mImage = (ImageView) findViewById(R.id.camera_image);
+		// 1
+		Intent intent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+		startActivityForResult(intent, CAMERA_PIC_REQUEST);
+	}
+
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		if (requestCode == CAMERA_PIC_REQUEST) {
+			Bitmap thumbnail = (Bitmap) data.getExtras().get("data");
+			mImage.setImageBitmap(thumbnail);
+			ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+			thumbnail.compress(Bitmap.CompressFormat.JPEG, 100, bytes);
+			thumbnail.setWidth(480);
+			thumbnail.setHeight(480);
+			File file = new File(Environment.getExternalStorageDirectory() + File.separator + "image.jpg");
+			try {
+				file.createNewFile();
+				FileOutputStream fo = new FileOutputStream(file);
+				fo.write(bytes.toByteArray());
+				fo.close();
+			} catch (IOException e) {
+				Log.wtf(this.getClass().getSimpleName(), "Could not save image", e);
+			}
+		}
+	}
+}
